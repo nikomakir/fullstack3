d71d7da284b90987781215f9af1cb8aeb81cecea
Niko Raulas <niko.raulas@outlook.com>
2024-10-01 01:18:13 +0300

    tehtävät 3.7 ja 3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', (request, response) => {
+     if (request.method === 'POST') {
+       return JSON.stringify(request.body)
+    } else { return '' }
+})
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 const generateId = () => String(Math.floor(Math.random()*500))
 
